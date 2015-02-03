e71a6ae383281b56a5e9569393d40ef95c4908c4
Kate <kbalducci@hotmail.com>
2015-02-02 23:00:16 -0600

    added admin path

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,7 +1,8 @@
 class Appointment < ActiveRecord::Base
   validates_presence_of :first_name, :last_name, :age, :birthdate,
                         :city, :state, :zipcode, :appt_time, :importance
-  validates_uniqueness_of :appt_time
+  validates_uniqueness_of :appt_time, message: "This time is not available, please pick another time"
   validates :age, numericality: { greater_than: 21}
   validates :importance, inclusion: { in: %w(High Low None)}
+  validates :zipcode, length: { is: 5 }
 end
